c917cc969c1679880c47f3bae0f073f65ebcb88d
David Le <david@walkerandcobrands.com>
2017-12-26 21:30:16 -0500

    Get rid of startup errors

--- a3c/environment.py
+++ a3c/environment.py
@@ -1,8 +1,13 @@
+#  from pysc2.env import environment
+from pysc2.env import sc2_env
+
+STEP_MUL = 16
+
 class Environment:
     def __init__(self):
+        env = sc2_env.SC2Env(
+                map_name="DefeatZerglingsAndBanelings",
+                step_mul=STEP_MUL,
+                visualize=True
+            )
         print("Init")
-
-    def run(self):
-        print("Begin run")
-        while True:
-            print("I'm running!")
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import sys
 from a3c.environment import Environment
+from absl import flags
 
-def main(argv):
-    Environment().run()
+def main():
+    # Load environment
+    env = Environment()
+
+    # Train
+
+def train():
+    print("Training")
+    # Init single agent
+
+    # Perform action from network
+
+    # Update network/collect rewards
 
 if __name__ == "__main__":
-    main(sys.argv)
+    flags.FLAGS(sys.argv)
+    main()
